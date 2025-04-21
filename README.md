7f251555461888b5e4533d1d6a33063487630bcf
nitinnaikwadi1 <nitinnaikwadi1@gmail.com>
2025-04-21 14:56:17 +0530

    url cleanup

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ A vedeo selection to view
 ## github pages deployable
 
 ### `flutter clean`
+### `flutter build web
+
+-- building with older flutter version before 3.24
 ### `flutter build web --web-renderer html`
--- lib/properties/app_constants.dart
+++ lib/properties/app_constants.dart
@@ -1,8 +1,8 @@
-const String vedeoAppBaseUrl = "https://nitinnaikwadi1.github.io/vedeobase/";
-const String vedeoAppVideoDataUrl = "${vedeoAppBaseUrl}data/vedeo_app/vedeo_app_videos_list.json";
-const String vedeoAppRandomAnimalsDataUrl = "${vedeoAppBaseUrl}data/vedeo_app/vedeo_app_random_animals_frames_gif_list.json";
-const String vedeoAppAnimalFramesUrl = "${vedeoAppBaseUrl}images/vedeo_app/random_animals_frames/";
-const String vedeoAppLandingFramesDataUrl = "${vedeoAppBaseUrl}data/vedeo_app/vedeo_app_landing_gif_list.json";
+const String allDataBaseUrl = "https://nitinnaikwadi1.github.io/vedeobase/";
+const String vedeoAppVideoDataUrl = "${allDataBaseUrl}data/vedeo_app/vedeo_app_videos_list.json";
+const String vedeoAppRandomAnimalsDataUrl = "${allDataBaseUrl}data/app_random_animals_frames_gif_list.json";
+const String vedeoAppAnimalFramesUrl = "${allDataBaseUrl}images/random_animals_frames/";
+const String vedeoAppLandingFramesDataUrl = "${allDataBaseUrl}data/vedeo_app/vedeo_app_landing_gif_list.json";
 const String vedeoAppAssetBase = "assets/images/";
 const String vedeoAppDashboardBackgcUrl = "${vedeoAppAssetBase}brand.jpg";
-const String vedeoAppLandingFrameUrl = "${vedeoAppBaseUrl}images/vedeo_app/landing/";
+const String vedeoAppLandingFrameUrl = "${allDataBaseUrl}images/vedeo_app/landing/";
